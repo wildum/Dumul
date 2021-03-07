6e4224b94f933b6297339d28fb2dc342093b2c74
William Dumont <william.dumont@isep.fr>
2021-03-07 17:04:37 +0100

    Menu first version

---
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void loadOneVsOne()
+    {
+        Debug.Log("load scene one v one");
+        SceneManager.LoadScene("Arena");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
