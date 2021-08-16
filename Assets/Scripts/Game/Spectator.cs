bc970f94af54c77375a38c521c7ad69e84cbff0a
William Dumont <william.dumont@isep.fr>
2021-08-16 22:10:10 +0200

    specators

--- Assets/Scripts/Game/Spectator.cs
+++ Assets/Scripts/Game/Spectator.cs
@@ -4,17 +4,90 @@ using UnityEngine;
 
 public class Spectator : MonoBehaviour
 {
+    public Transform lefthand;
+    public Transform rightHand;
 
+    const float maxMovement = 0.5f;
+    const float movementSpeed = 2;
+    const float movementSpeedHand = 5;
     private int team;
+    private float time = 0;
+    private float exitedTime = 0;
+    private bool exited = false;
+    private bool upDirection = true;
+    private Vector3 initialPosition;
+    private Vector3 movedPosition;
+
+    private Vector3 leftHandPositionInit;
+    private Vector3 rightHandPositionInit;
+
+    private Vector3 leftHandMovedPosition;
+    private Vector3 rightHandMovedPosition;
 
     void Start()
     {
-
+        initialPosition = transform.position;
+        movedPosition = new Vector3(initialPosition.x, initialPosition.y + maxMovement, initialPosition.z);
+        leftHandPositionInit = lefthand.localPosition;
+        rightHandPositionInit = rightHand.localPosition;
+        leftHandMovedPosition = new Vector3(leftHandPositionInit.x, leftHandPositionInit.y + 0.5f, leftHandPositionInit.z + 0.1f);
+        rightHandMovedPosition = new Vector3(rightHandPositionInit.x, rightHandPositionInit.y + 0.5f, rightHandPositionInit.z - 0.1f);
     }
 
     void Update()
     {
-        
+        if (exited)
+        {
+            lefthand.localPosition = Vector3.Lerp(lefthand.localPosition, leftHandMovedPosition, Time.deltaTime * movementSpeedHand);
+            rightHand.localPosition = Vector3.Lerp(rightHand.localPosition, rightHandMovedPosition, Time.deltaTime * movementSpeedHand);
+
+            if (upDirection)
+            {
+                transform.position = Vector3.Lerp(transform.position, movedPosition, Time.deltaTime * movementSpeed);
+                if (isAlmostEqualY(transform.position, movedPosition))
+                {
+                    upDirection = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, initialPosition, Time.deltaTime * movementSpeed);
+                if (isAlmostEqualY(transform.position, initialPosition))
+                {
+                    upDirection = true;
+                }
+            }
+
+            time += Time.deltaTime;
+            if (time > exitedTime)
+            {
+                time = 0;
+                exited = false;
+            }
+        }
+        else
+        {
+            lefthand.localPosition = Vector3.Lerp(lefthand.localPosition, leftHandPositionInit, Time.deltaTime * movementSpeedHand);
+            rightHand.localPosition = Vector3.Lerp(rightHand.localPosition, rightHandPositionInit, Time.deltaTime * movementSpeedHand);
+
+            if (!isAlmostEqualY(transform.position, initialPosition))
+            {
+                upDirection = true;
+                transform.position = Vector3.Lerp(transform.position, initialPosition, Time.deltaTime * movementSpeed);
+            }
+        }
+    }
+
+    private bool isAlmostEqualY(Vector3 v, Vector3 u)
+    {
+        return Mathf.Abs(v.y - u.y) < 0.1f;
+    }
+
+    public void setExitedState(float iexitedTime)
+    {
+        exited = true;
+        time = 0;
+        exitedTime = iexitedTime;
     }
 
     public int Team { set { team = value; } get { return team;} }
--- Assets/Scripts/Game/SpectatorsManager.cs
+++ Assets/Scripts/Game/SpectatorsManager.cs
@@ -4,21 +4,59 @@ using UnityEngine;
 
 public class SpectatorsManager : MonoBehaviour
 {
+    const float exitedTime = 5; // seconds
+    const float hysterese = 2; // seconds
+    const float probExitementBase = 0.2f;
+    const float freqExitement = 5;
 
     public GameObject prefab;
     public Material matBlue;
     public Material matOrange;
 
-    private Spectator[] spectators = new Spectator[210];
+    private List<Spectator> blueSpectators = new List<Spectator>();
+    private List<Spectator> orangeSpectators = new List<Spectator>();
+
     private float[] zPos = {-12.131f, -12.616f, -13.1f, -13.5f, -13.87f, -13.987f, -14.115f, -14.184f, -14.184f, -14.184f, -14.184f,
                             -14.184f, -14.184f, -14.184f, -14.115f, -13.987f, -13.87f, -13.5f, -13.1f, -12.616f, -12.131f};
     private int specPerLine = 21;
     private int specPerColumn = 5;
     private int sides = 2;
 
+    private float probExitementBlue = probExitementBase;
+    private float probExitementOrange = probExitementBase;
+
+    private float time = 0;
+
     private static System.Random rnd = new System.Random();
 
     void Start()
+    {
+        initSpectators();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > freqExitement)
+        {
+            time = 0;
+            exitSpectators(blueSpectators, probExitementBlue);
+            exitSpectators(orangeSpectators, probExitementOrange);
+        }
+    }
+
+    void exitSpectators(List<Spectator> spectators, float probExitement)
+    {
+        foreach (Spectator s in spectators)
+        {
+            if (rnd.NextDouble() < probExitement)
+            {
+                s.setExitedState(exitedTime + ((float) rnd.NextDouble() - 0.5f) * hysterese * 2.0f);
+            }
+        }
+    }
+
+    void initSpectators()
     {
         for (int i = 0; i < specPerLine; i++)
         {
@@ -30,19 +68,21 @@ public class SpectatorsManager : MonoBehaviour
                         prefab.transform.position.y - 1.5f*j,
                         zPos[i] * (side == 0 ? 1.0f : -1.0f)),
                         Quaternion.identity);
-                    int index = i*specPerColumn*sides + j*sides + side;
-                    spectators[index] = obj.GetComponent<Spectator>();
+
+                    obj.transform.Rotate(0, 90 * (side == 0 ? -1 : 1), 0);
+
+                    Spectator s = obj.GetComponent<Spectator>();
                     int team = rnd.NextDouble() < 0.5 ? 0 : 1;
-                    spectators[index].Team = team;
-                    obj.GetComponentInChildren<Renderer>().material = team == 0 ? matOrange : matBlue;
+                    s.Team = team;
+                    if (team == 0)
+                        orangeSpectators.Add(s);
+                    else
+                        blueSpectators.Add(s);
+                    foreach(var child in obj.GetComponentsInChildren<Renderer>())
+                        child.material = team == 0 ? matOrange : matBlue;
                 }
             }
         }
         Destroy(prefab);
-    }
-
-    void Update()
-    {
-        
     }
 }
