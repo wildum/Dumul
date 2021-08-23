f771a4e188240a07769c654353ef2651bbe9342b
William Dumont <william.dumont@isep.fr>
2021-08-24 00:53:40 +0200

    rework thunder animation

--- Assets/Scripts/Spells/ThunderParent.cs
+++ Assets/Scripts/Spells/ThunderParent.cs
@@ -10,6 +10,18 @@ public class ThunderParent : MonoBehaviour, IPunInstantiateMagicCallback
         object[] instantiationData = info.photonView.InstantiationData;
         int team = (int) instantiationData[0];
         GetComponentInChildren<Thunder>().Team = team;
-        GetComponentInChildren<ParticleSystemRenderer>().trailMaterial = GameSettings.getElectricalMaterial(team);
+        foreach (var particles in GetComponentsInChildren<ParticleSystemRenderer>())
+        {
+            particles.trailMaterial = GameSettings.getElectricalMaterial(team);
+        }
+        foreach (var ps in GetComponentsInChildren<ParticleSystem>())
+        {
+            var col = ps.colorOverLifetime;
+            col.enabled = true;
+            Gradient grad = new Gradient();
+            Color teamColor = team == 0 ? new Color(1, 0.6f, 0.34f) : new Color(0.34f, 0.36f, 1);
+            grad.SetKeys( new GradientColorKey[] { new GradientColorKey(teamColor, 1.0f), new GradientColorKey(Color.white, 0.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) } );
+            col.color = grad;
+        }
     }
 }
